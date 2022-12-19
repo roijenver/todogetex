45be6b534570ca3bb3725efdac1da73892617d42
roijenver <roijenvercabaluna@gmail.com>
2022-12-19 22:55:06 +0800

    Added completed list_page.dart

---
+++ lib/list_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/todo_details.dart';
+import 'package:http/http.dart';
+import 'dart:convert' as convert;
+
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+
+  List todos = <dynamic>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getTodo();
+  }
+
+  getTodo() async {
+var url = 'https://jsonplaceholder.typicode.com/todos';
+var response = await http.get(Uri.parse(url));
+
+setState(() {
+  todos = convert.jsonDecode(response.body) as List<dynamic>;
+
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('API'),
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              title: Text(todos[index]['title']),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => TodoDetails(todo: todos[index])
+
+                )
+              );
+            }
+          );
+        }
+      ),
+    );
+  }
+}
