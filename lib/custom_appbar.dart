c36f281d39bf2271f6466521afb52b30df8cfcc5
roijenver <roijenvercabaluna@gmail.com>
2022-12-19 22:39:49 +0800

    Added completed custom_appbar.dart

---
+++ lib/custom_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar  extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 50,
+        color: Colors.orange,
+    );
+  }
+}
